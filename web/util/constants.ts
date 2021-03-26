3c5a9abd8cf2c338bfa997537397c86a5d0c014d
Carter Himmel <carter@elhnet.net>
2021-03-26 00:00:58 -0600

    chore: update api url

--- web/util/constants.ts
+++ web/util/constants.ts
@@ -1,2 +1,2 @@
 export const API_URL =
-	process.env.NODE_ENV === 'production' ? 'https://api.stockx-gif.fyko.net' : 'http://localhost:2319';
+	process.env.NODE_ENV === 'production' ? 'https://api-stockx-gif.fyko.net' : 'http://localhost:2319';
